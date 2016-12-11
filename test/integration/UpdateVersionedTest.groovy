d0748238b92ab86b1b3e002fbe43c34435ec69bc
aistomin <andrej.istomin.ikeen@gmail.com>
2016-12-11 01:06:14 +0100

    the workaround test

--- test/integration/UpdateVersionedTest.groovy
+++ test/integration/UpdateVersionedTest.groovy
@@ -1,12 +1,15 @@
+import org.hibernate.Session
+
 /**
  * Created by aistomin on 10/12/2016.
  *
- * Test that demonstrates the "UPDATE VERSIONED" queries bug in GORM/Hibernate.
+ * This tests suite demonstrates the "UPDATE VERSIONED" queries bug in
+ * GORM/Hibernate and one of the possible workarounds.
  */
 class UpdateVersionedTest extends GroovyTestCase {
 
     /**
-     * Check "UPDATE VERSIONED" HQL query.
+     * This test demonstrates the bug.
      */
     void testUpdateVersionedBug() {
         final def entity = new Child(name: 'Test').save()
@@ -15,6 +18,23 @@ class UpdateVersionedTest extends GroovyTestCase {
             'UPDATE VERSIONED Parent p SET p.name = :val WHERE p.id = :id',
             [id: entity.id, val: 'Updated value']
         )
+        assertEquals(version + 1, entity.refresh().version)
+    }
+
+    /**
+     * This test demonstrates the workaround.
+     */
+    void testUpdateVersionedBugWorkaround() {
+        final def entity = new Child(name: 'Test').save()
+        final def version = entity.version
+        Parent.withSession { final Session session ->
+            final def query = session.createSQLQuery(
+                'UPDATE parent p SET p.name = :val, p.version = p.version + 1 WHERE p.id = :id'
+            )
+            query.setLong('id', entity.id)
+            query.setString('val', 'Updated value')
+            query.executeUpdate()
+        }
         assertEquals(version + 1, entity.refresh().version)
     }
 }
